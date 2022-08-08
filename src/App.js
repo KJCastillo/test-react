d1dc404c501c37f48c8fc554ddeba874dbe8a4c7
Kevin Castillo <kevincastillo318@gmail.com>
2022-08-08 15:52:36 -0700

    state array testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { useState } from 'react';
 function App() {
   
   const [name, setName] = useState('Kevin')
+  const [events, setEvents] = useState([
+    {title: 'study', id: 1},
+    {title: 'work', id: 2},
+    {title: 'fitness', id: 3}
+  ]) 
 
   const handleClick = () => {
     setName('Penny')
@@ -14,6 +19,13 @@ function App() {
     <div className="App">
       <h1>my name is {name}</h1>
       <button onClick={handleClick}>change name</button>
+      {events.map((event) => (
+        <div key={event.id}>
+          <h2>
+            {event.title}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
