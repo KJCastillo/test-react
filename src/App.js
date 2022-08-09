592e5c834d715f371b4e25dbd60d7a0630fca843
Kevin Castillo <kevincastillo318@gmail.com>
2022-08-09 14:13:24 -0700

    show and hide event buttons

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [showEvents, setShowEvents] = useState(true)
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
-    {title: 'study', id: 1},
-    {title: 'work', id: 2},
-    {title: 'fitness', id: 3}
-  ]) 
+    { title: "study", id: 1 },
+    { title: "work", id: 2 },
+    { title: "fitness", id: 3 },
+  ]);
 
   const handleClick = (id) => {
-   setEvents((prevEvents) => {
-    return prevEvents.filter((event) => {
-      return id !== event.id
-    })
-   })
-    console.log(id)
-  }
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
+  };
 
   return (
     <div className="App">
-      <div>
-        <button onClick={() => setShowEvents(false)}>hide events</button>
-      </div>
-      <div>
-        <button onClick={() => setShowEvents(true)}>show events</button>
-      </div>
-      {showEvents && events.map((event, index) => (
-        <div key={event.id}>
-          <h2>{index} - 
-            {event.title} 
-            <button onClick={() => {handleClick(event.id)}}>delete event</button>
-          </h2>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
         </div>
-      ))}
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index} -{event.title}
+              <button
+                onClick={() => {
+                  handleClick(event.id);
+                }}
+              >
+                delete event
+              </button>
+            </h2>
+          </div>
+        ))}
     </div>
   );
 }
