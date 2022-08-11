d48d96aaf8f78988136abcbc6012139b4ab148a9
Kevin Castillo <kevincastillo318@gmail.com>
2022-08-11 15:31:24 -0700

    inline styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,19 @@
-import './Modal.css'
+import "./Modal.css";
 
-export default function Modal({children, handleClose}) {
+export default function Modal({ children, handleClose }) {
   return (
     <div className="modal-backdrop">
-        <div className="modal">
-           {children}
-           <button onClick={handleClose}>close</button>
-        </div>
+      <div
+        className="modal"
+        style={{
+          border: "4px solid",
+          borderColor: "tomato",
+          textAlign: "center",
+        }}
+      >
+        {children}
+        <button onClick={handleClose}>close</button>
+      </div>
     </div>
-  )
+  );
 }
