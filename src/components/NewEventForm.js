089423b4085e026104c927e64b8d4687149b5b30
Kevin Castillo <kevincastillo318@gmail.com>
2022-08-15 16:11:20 -0700

    select box for date

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -4,10 +4,12 @@ import "./NewEventForm.css";
 export default function NewEventForm({ addEvent }) {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
+  const [location, setLocation] = useState('San Diego')
 
   const resetForm = () => {
     setTitle("");
     setDate("");
+    setLocation("San Diego")
   };
 
   const handleSubmit = (e) => {
@@ -16,12 +18,16 @@ export default function NewEventForm({ addEvent }) {
     const event = {
       title: title,
       date: date,
+      location: location,
       id: Math.floor(Math.random() * 10000)
+      
     }
     addEvent(event)
     resetForm()
+    console.log(event)
   }
 
+  
   return (
     <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
@@ -39,6 +45,14 @@ export default function NewEventForm({ addEvent }) {
           onChange={(e) => setDate(e.target.value)}
           value={date}
         />
+      </label>
+      <label>
+        <span>Event location:</span>
+        <select onChange={(e) => setLocation(e.target.value)}>
+          <option value="miami">Miami</option>
+          <option value="kauai">Kauai</option>
+          <option value="austin">Austin</option>
+        </select>
       </label>
       <button>Submit</button>
     </form>
